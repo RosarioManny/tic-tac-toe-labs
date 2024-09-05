b78c21a99709f54419fbe84c19b8f9e09e2ba8c8
RosarioManny <rosario.emm47@gmail.com>
2024-09-05 16:40:25 -0400

    Exercise 1-4b

--- js/app.js
+++ js/app.js
@@ -1,20 +1,51 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
-
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+let board = ['X', '', 'O', '', '', 'X', '', 'O', ''];
+let turn = "X";
+let tie = false;
+let winner = false; 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.querySelector("#message");
 /*-------------------------------- Functions --------------------------------*/
+const init = (event) => {
+    render(event)
+};
 
+const render = () => {
+}
 
+const updateBoard = (boardEl) => {
+    for(let i = 0; i < boardEl.length; i++) {
+        squareEls[i].textContent = boardEl[i];
+        console.log(squareEls[i]);
+    }
+} 
+console.log(updateBoard(board));
 
+const updateMessage = () => {
+}
+
+//console.log(updateBoard("O"))
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
+
+/*----------------------------- PsuedoCode -----------------------------------*/
+
+/* 1) Define the required variables used to track the state of the game.
+
+2) Store cached element references.
+
+3) Upon loading, the game state should be initialized, and a function should 
+  be called to render this game state.
+
+4) The state of the game should be rendered to the user.
+
+5) Define the required constants.
+
+6) Handle a player clicking a square with a `handleClick` function.
+
+7) Create Reset functionality */
