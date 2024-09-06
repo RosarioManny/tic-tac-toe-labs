371732614086eb35eadb08b0df6da7435d13ab01
RosarioManny <rosario.emm47@gmail.com>
2024-09-06 09:48:14 -0400

    Adjustments

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-/*-------------------------------- Constants --------------------------------*/
+// /*-------------------------------- Constants --------------------------------*/
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.querySelector("#message");
 
-/*---------------------------- Variables (state) ----------------------------*/
-let board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+// /*---------------------------- Variables (state) ----------------------------*/
+let board = ['1', '2', 'X', '4', '5', '6', '7', '8', '9'];
 let turn = "X";
 let tie = false;
 let winner = false; 
-/*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll(".sqr");
-const messageEl = document.querySelector("#message");
-/*-------------------------------- Functions --------------------------------*/
+// /*------------------------ Cached Element References ------------------------*/
+
+// /*-------------------------------- Functions --------------------------------*/
 const init = () => {
     render()
 };
 
+// const squareElPres = squareEls('click', () => {
+//     console.log('you clikt me')
+// })
+
+// const handleClick = (event) => {
+//     console.log("Clicked BBV")}
 const updateBoard = () => {
     for(let idx = 0; idx < board.length; idx++) {
         squareEls[idx].textContent = board[idx];
-        console.log(squareEls[idx]);
+        (squareEls[idx].textContent);
     }
 } 
-// console.log(updateBoard(board));
+
+// console.log(updateBoard(""))
 
 
-const updateMessage = (messageEl) => {
+const updateMessage = () => {
     if (tie === false && winner === false) {
         messageEl = "Next Player"
     } else if (tie === true && winner === false) {
@@ -33,41 +41,21 @@ const updateMessage = (messageEl) => {
     console.log(messageEl)
 }
 
-const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5], 
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7],
-    [2, 5, 8], 
-    [0, 4, 7],
-    [2, 4, 6],
-]
+// const winningCombos = [
+//     [0, 1, 2],
+//     [3, 4, 5], 
+//     [6, 7, 8],
+//     [0, 3, 6], 
+//     [1, 4, 7],
+//     [2, 5, 8], 
+//     [0, 4, 7],
+//     [2, 4, 6],
+// ]
 
 const render = () => {
     updateBoard()
     updateMessage()
 }
 
-console.log(render())
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
-
-/*----------------------------- PsuedoCode -----------------------------------*/
-
-/* 1) Define the required variables used to track the state of the game.
-
-2) Store cached element references.
-
-3) Upon loading, the game state should be initialized, and a function should 
-  be called to render this game state.
-
-4) The state of the game should be rendered to the user.
-
-5) Define the required constants.
-
-6) Handle a player clicking a square with a `handleClick` function.
-
-7) Create Reset functionality */
+console.log(updateBoard)
+// /*----------------------------- Event Listeners -----------------------------*/
